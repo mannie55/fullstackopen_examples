16229136681c828d31f96069ba28787db844439a
nnamdi <mannnie55@gmail.com>
2025-07-21 12:43:11 +0100

    add: dummy server  to fetch data to frontend

--- part2-notes-frontend/src/App.jsx
+++ part2-notes-frontend/src/App.jsx
@@ -1,12 +1,26 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import Note from './components/Note'
+import noteService from './services/notes'
 
 
-const App = (props) => {
-  const [notes, setNotes] = useState(props.notes)
+const App = () => {
+  const [notes, setNotes] = useState([])
   const [newNote, setNewNote] = useState('')
   const [showAll, setShowAll] = useState(true)
 
+
+ const hook = () => {
+    noteService
+      .getAll()
+      .then(initialNotes => {
+        setNotes(initialNotes)
+      })
+  }
+
+  useEffect(hook, [])
+
+  console.log('render', notes.length, 'notes');
+  
 
   const notesToShow = showAll ? notes : notes.filter(note => note.important)
 
@@ -17,10 +31,15 @@ const App = (props) => {
       important: Math.random() < 0.5,
       id: String(notes.length + 1),
     }
-    console.log(noteObject);
+
+    noteService
+      .create(noteObject)
+      .then(returnedNote => {
+        setNotes(notes.concat(returnedNote))
+        setNewNote('')
+      })
     
-    setNotes(notes.concat(noteObject))
-    setNewNote('')
+    
   }
 
   const handleNoteChange = (event) => {
@@ -35,6 +54,26 @@ const App = (props) => {
 
   const show = showAll ? 'important' : 'all'
 
+
+  const toggleImportanceOf = (id) => {
+    const note = notes.find(n => n.id === id)
+    const changeNote = { ...note, important: !note.important }
+
+    noteService
+      .update(id, changeNote)
+      .then(returnedNote => {
+        setNotes(notes.map(note => note.id === id ? returnedNote : note))
+    })
+      .catch(error => {
+        alert(
+          `the note '${note.content}' was already deleted from the server`
+        )
+        setNotes(notes.filter(n => n.id !== id))
+      })
+
+  }
+
+
   return (
     <div>
       <h1>Notes</h1>
@@ -45,7 +84,9 @@ const App = (props) => {
       </div>
       <ul>
         {notesToShow.map(note =>
-        <Note key={note.id} note={note}/>
+        <Note key={note.id} note={note}
+        toggleImportance={() => toggleImportanceOf(note.id)}
+        />
         )}
       
       </ul>

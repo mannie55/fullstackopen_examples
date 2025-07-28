929d00d1b6e1ffe5033574862a0a4085fad1f1a1
nnamdi <mannnie55@gmail.com>
2025-07-28 09:55:20 +0100

    added dist dir for production

--- backend/index.js
+++ backend/index.js
@@ -2,6 +2,7 @@ const express = require('express')
 const morgan = require('morgan')
 const app = express()
 
+app.use(express.static('dist'))
 
 
 const cors = require('cors')
--- part2-notes-frontend/src/services/notes.js
+++ part2-notes-frontend/src/services/notes.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 
-const baseUrl = 'http://localhost:3001/api/notes'
+const baseUrl = '/api/notes'
 
 const getAll = () => {
     const request = axios.get(baseUrl)
